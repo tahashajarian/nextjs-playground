658edf8aba85a4c0616813d2494bb30408d8191c
طاها شجریان <t.shajarian@pdnco>
2021-03-10 17:40:16 +0330

    footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -29,23 +29,17 @@ export const Footer = (props: Props) => {
         </div>
         <div>
           <legend className={style.legend}>
-            <Image src="/images/icon.png" height={60} width={60} alt="logo" />
+            <img className={style.icon} src="/images/logo2.svg" />
+            <img className={style.dotsRight} src="/images/dots10.svg" />
+            <img className={style.dotsLeft} src="/images/dots10.svg" />
           </legend>
-          <ul>
-            <span className={style.title}>تماس با ما</span>
-            <li>
-              <span>icon</span>
-              <span className={"text"}>alkdjflaksdjfalsdfjsl;dfj</span>
-            </li>
-            <li>
-              <span>icon</span>
-              <span className={"text"}>alkdjflaksdjfalsdfjsl;dfj</span>
-            </li>
-            <li>
-              <span>icon</span>
-              <span className={"text"}>alkdjflaksdjfalsdfjsl;dfj</span>
-            </li>
-          </ul>
+          <div className={style.copyRight}>
+            <span>
+              کلیه حقوق مادی و معنوی این وبسایت متعلق به سامانه اعتبار من بوده و
+              هرگونه استفاده از مطالب آن غیر مجاز است
+            </span>
+            <span>© 2020-2021 Etebareman.ir. All rights reserved.</span>
+          </div>
         </div>
         <div>
           <ul>
