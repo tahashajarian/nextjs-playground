334972aefd2241f67876d5c376576ac3c8457bb0
طاها شجریان <t.shajarian@pdnco>
2021-05-01 22:29:40 +0430

    add otp

--- containers/otp/otp-component.tsx
+++ containers/otp/otp-component.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./otp.module.scss";
 import { useRouter } from "next/router";
 import Input from "../../components/input/input";
 import utils from "../../components/utils.module.scss";
 
 const Otp = ({ post }) => {
+  const timeWait = 120;
+  const [timer, setTimer] = useState(timeWait);
+  const [status, setStatus] = useState<any>();
+  const [nationalCode, setNationalCode] = useState<any>();
+  const [phoneNumber, setphoneNumber] = useState<any>();
   const route = useRouter();
-  const url = new URL("http://example" + route.asPath);
-  const status = url.searchParams.get("status");
-  const nationalCode = url.searchParams.get("nationalCode");
-  const phoneNumber = url.searchParams.get("phoneNumber");
-  console.log(status);
+  useEffect(() => {
+    const url = new URL("http://example" + route.asPath);
+    setStatus(url.searchParams.get("status"));
+    setNationalCode(url.searchParams.get("nationalCode"));
+    setphoneNumber(url.searchParams.get("phoneNumber"));
+    startTimer();
+  }, []);
+
+  const startTimer = () => {
+    setInterval(() => {
+      // setTimer(() => timer - 1);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (timer > 0) {
+      setTimeout(() => {
+        setTimer((timer) => timer - 1);
+      }, 1000);
+    }
+  }, [timer]);
+
+  const resend = () => {
+    setTimer(timeWait);
+  };
 
   return (
     <div className={style.container}>
@@ -39,9 +64,19 @@ const Otp = ({ post }) => {
           width={"400px"}
         />
       </div>
-      <div>
-        <span></span>
-        <span></span>
+      <div className={style.timer}>
+        {timer > 0 ? (
+          <div>{`0${Math.trunc(timer / 60)}:${
+            timer % 60 < 10 ? "0" + (timer % 60) : timer % 60
+          }`}</div>
+        ) : (
+          <div>
+            <span>کد را دریافت نکردید؟</span>
+            <span className={style.resend} onClick={resend}>
+              ارسال مجدد
+            </span>
+          </div>
+        )}
       </div>
       <div className={style.submit}>
         <button className={`${utils.lgButtonPrimary} ${utils.shadowPrimary}`}>
