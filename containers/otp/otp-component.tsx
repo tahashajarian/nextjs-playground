802301fcc4b2a6a4af0dc4a2d57552d8638936d2
taha <shajariantaha@gmial.com>
2021-05-02 17:25:17 +0430

    cmp otp

--- containers/otp/otp-component.tsx
+++ containers/otp/otp-component.tsx
@@ -3,6 +3,8 @@ import style from "./otp.module.scss";
 import { useRouter } from "next/router";
 import Input from "../../components/input/input";
 import utils from "../../components/utils.module.scss";
+import Link from "next/link";
+import { routes } from "../../constants/routes";
 
 const Otp = ({ post }) => {
   const timeWait = 120;
@@ -16,14 +18,7 @@ const Otp = ({ post }) => {
     setStatus(url.searchParams.get("status"));
     setNationalCode(url.searchParams.get("nationalCode"));
     setphoneNumber(url.searchParams.get("phoneNumber"));
-    startTimer();
   }, []);
-
-  const startTimer = () => {
-    setInterval(() => {
-      // setTimer(() => timer - 1);
-    }, 1000);
-  };
 
   useEffect(() => {
     if (timer > 0) {
@@ -45,44 +40,60 @@ const Otp = ({ post }) => {
         ) : (
           <span className={style.fail}>پرداخت انجام نشد</span>
         )}
-        <p>
-          گزارش اعتباری برای شماره ملی
-          <b> {nationalCode} </b>و شماره همراه
-          <b> {phoneNumber} </b>
-          آماده دریافت است لطفا کد یکبار مصرف که به شماره همراه ثبت شده ارسال
-          شده در کادر زیر وارد نمایید سپس دکمه
-          <b> دانلود گزارش اعتباری </b>
-          را بزنید
-        </p>
-      </div>
-      <div>
-        <Input
-          label={""}
-          onChange={() => {}}
-          placeholder={"کد یک بار مصرف را وارد کنید"}
-          value={""}
-          width={"400px"}
-        />
-      </div>
-      <div className={style.timer}>
-        {timer > 0 ? (
-          <div>{`0${Math.trunc(timer / 60)}:${
-            timer % 60 < 10 ? "0" + (timer % 60) : timer % 60
-          }`}</div>
+        {status !== "true" ? (
+          <p></p>
         ) : (
-          <div>
-            <span>کد را دریافت نکردید؟</span>
-            <span className={style.resend} onClick={resend}>
-              ارسال مجدد
-            </span>
-          </div>
+          <p>
+            گزارش اعتباری برای شماره ملی
+            <b> {nationalCode} </b>و شماره همراه
+            <b> {phoneNumber} </b>
+            آماده دریافت است لطفا کد یکبار مصرف که به شماره همراه ثبت شده ارسال
+            شده در کادر زیر وارد نمایید سپس دکمه
+            <b> دانلود گزارش اعتباری </b>
+            را بزنید
+          </p>
         )}
       </div>
-      <div className={style.submit}>
-        <button className={`${utils.lgButtonPrimary} ${utils.shadowPrimary}`}>
-          تایید و مشاهده هزینه گزارش
-        </button>
-      </div>
+      {status === "true" ? (
+        <div>
+          <div>
+            <Input
+              label={""}
+              onChange={() => {}}
+              placeholder={"کد یک بار مصرف را وارد کنید"}
+              value={""}
+              width={"400px"}
+            />
+          </div>
+          <div className={style.timer}>
+            {timer > 0 ? (
+              <div>{`0${Math.trunc(timer / 60)}:${
+                timer % 60 < 10 ? "0" + (timer % 60) : timer % 60
+              }`}</div>
+            ) : (
+              <div>
+                <span>کد را دریافت نکردید؟</span>
+                <span className={style.resend} onClick={resend}>
+                  ارسال مجدد
+                </span>
+              </div>
+            )}
+          </div>
+          <div className={style.submit}>
+            <button
+              className={`${utils.lgButtonPrimary} ${utils.shadowPrimary}`}
+            >
+              ارسال
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Link href={routes.home}>
+          <button className={`${utils.lgButtonPrimary} ${utils.shadowPrimary}`}>
+            بازگشت به صفحه اصلی{" "}
+          </button>
+        </Link>
+      )}
     </div>
   );
 };
