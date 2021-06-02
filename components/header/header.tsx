8c0c7ae00e9dbda6ed92dee786bd902d20f56706
طاها شجریان <t.shajarian@pdnco>
2021-06-02 18:25:42 +0430

    menu responsive

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./header.module.scss";
 import utils from "../utils.module.scss";
 import Link from "next/link";
@@ -7,35 +7,118 @@ interface Props {
   pages: any[];
 }
 
-const Header = (props: Props) => (
-  <header className={style.header}>
-    <Link href="/">
-      <span className={style.icon}>
-        <Image
-          src="/images/logo.svg" // Route of the image file
-          height={50} // Desired size with correct aspect ratio
-          width={50} // Desired size with correct aspect ratio
-          alt="Your Name"
-        />
-        <span>اعتبار من</span>
-      </span>
-    </Link>
-    <ul className={style.menus}>
-      {props.pages.map((page, index) => (
-        <li key={index}>
-          <Link href={page.path}>
-            <a className={utils.navLink}>{page.title}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-    <div className={style.loginActions}>
-      <ul>
-        <li className={utils.mdButtonPrimaryOutline}>ورود به سامانه</li>
-        <li className={utils.mdButtonSecondry}>عضویت در سامانه</li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = (props: Props) => {
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <header className="">
+      <div className="hidden lg:flex pt-2">
+        <Link href="/">
+          <span className="flex items-center">
+            <img
+              src="/images/logo.svg" // Route of the image file
+              height={50} // Desired size with correct aspect ratio
+              width={50} // Desired size with correct aspect ratio
+              alt="Your Name"
+            />
+            <span className="whitespace-nowrap text-brandPrimary-main pr-2">
+              اعتبار من
+            </span>
+          </span>
+        </Link>
+        <ul className="flex flex-row lg:mx-10 mx-5 justify-center items-center">
+          {props.pages.map((page, index) => (
+            <li key={index} className="mx-2 flex justify-center items-center">
+              <Link href={page.path}>
+                <a className="whitespace-nowrap text-gray-500 lg:text-sm text-xs">
+                  {page.title}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="">
+          <ul className="flex flex-row">
+            <li className="btn btn-primary-outline-sm ml-5">ورود به سامانه</li>
+            <li className="btn btn-secondray-sm">عضویت در سامانه</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="lg:hidden flex">
+        <div className="w-full flex flex-row justify-center items-center bg-white shadow-md h-12 fixed z-40">
+          <span
+            onClick={() => {
+              setOpenMenu(true);
+            }}
+            className=" flex flex-col justify-center items-center ml-auto w-10 h-full cursor-pointer pr-2"
+          >
+            <img src="/images/menu-icon.svg" alt="menu icon" />
+          </span>
+          <div className="w-full flex justify-center">
+            <Link href="/">
+              <span className="flex items-center">
+                <img
+                  src="/images/logo.svg" // Route of the image file
+                  alt="etebareman"
+                  className="h-9"
+                />
+                <span className="whitespace-nowrap text-brandPrimary-main pr-2">
+                  اعتبار من
+                </span>
+              </span>
+            </Link>
+          </div>
+          <li className="btn btn-primary-outline-sm ml-1">ورود به سامانه</li>
+        </div>
+      </div>
+      {openMenu && (
+        <div className="fixed z-50 bg-gray-400 bg-opacity-50 inset-0">
+          <div className="absolute h-full bg-white flex flex-col items-center py-10 shadow-lg w-64">
+            <span
+              className="cursor-pointer absolute top-5 left-5"
+              onClick={() => {
+                setOpenMenu(false);
+              }}
+            >
+              &#10005;
+            </span>
+            <Link href="/">
+              <span className="flex items-center">
+                <img
+                  src="/images/logo.svg" // Route of the image file
+                  height={50} // Desired size with correct aspect ratio
+                  width={50} // Desired size with correct aspect ratio
+                  alt="Your Name"
+                />
+                <span className="whitespace-nowrap text-brandPrimary-main pr-2">
+                  اعتبار من
+                </span>
+              </span>
+            </Link>
+            <ul className="flex flex-col justify-center items-center my-8">
+              {props.pages.map((page, index) => (
+                <li
+                  key={index}
+                  className="my-4 flex justify-center items-center"
+                >
+                  <Link href={page.path}>
+                    <a className="whitespace-nowrap text-gray-500 text-xs">
+                      {page.title}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <hr />
+            <ul className="flex flex-row my-5 w-full justify-around">
+              <li className="btn btn-primary-outline-sm">ورود به سامانه</li>
+              <li className="btn btn-secondray-sm">عضویت در سامانه</li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandPrimary: {
+          light: "#61acff",
+          main: "#0078ff",
+          dark: "#005abf",
+        },
+        brandSecondary: {
+          light: "#6febd4",
+          main: "#1ed6b5",
+          dark: "#169981",
+        },
+        tertiary: {
+          DEFAULT: "#fe8075",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
